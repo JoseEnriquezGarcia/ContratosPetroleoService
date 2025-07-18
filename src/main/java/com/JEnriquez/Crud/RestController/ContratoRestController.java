925e8cd7b0383893cb049c955e611ba36b515142
Jesús Adrian Sanchez <jasanrod00@gmail.com>
2025-07-17 18:54:04 -0600

    ContratosService

--- src/main/java/com/JEnriquez/Crud/RestController/ContratoRestController.java
+++ src/main/java/com/JEnriquez/Crud/RestController/ContratoRestController.java
@@ -1,10 +1,13 @@
 package com.JEnriquez.Crud.RestController;
 
 import com.JEnriquez.Crud.DAO.IContratoDAO;
+import com.JEnriquez.Crud.JPA.Contrato;
 import com.JEnriquez.Crud.JPA.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,6 +61,29 @@ public class ContratoRestController {
             }
         } else {
             return ResponseEntity.badRequest().body(result.errorMessage);
+        }
+    }
+    
+    @PostMapping("/busquedaContratoService")
+    public ResponseEntity BusquedaContratos(@RequestBody Contrato contrato){
+        Result result = new Result();
+        try {
+            result.objects = iContratoDAO.findAll();
+            result.correct = true;
+        } catch (Exception ex) {
+            result.correct = false;
+            result.errorMessage = ex.getLocalizedMessage();
+            result.ex = ex;
+        }
+        
+        if(result.correct){
+            if(!result.objects.isEmpty()){
+                return ResponseEntity.ok().body(result);
+            }else{
+                return ResponseEntity.noContent().build();
+            }
+        }else{
+            return ResponseEntity.internalServerError().body(result.errorMessage);
         }
     }
 }
